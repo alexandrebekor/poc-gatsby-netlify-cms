1354c21d312eb6ba238ae6b44f16130bfc1a8b62
Alexandre Bekor <contato@alexandrebekor.com>
2021-11-22 17:06:17 -0300

    Include templates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,20 @@
 module.exports = {
     plugins: [
-        `gatsby-plugin-netlify-cms`
+        `gatsby-plugin-netlify-cms`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `author`,
+                path: __dirname + `/src/content/author`
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `post`,
+                path: __dirname + `/src/content/post`
+            }
+        },
+        `gatsby-transformer-remark`
     ]
 }
